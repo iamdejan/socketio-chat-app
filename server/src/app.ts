bf35ea7faaa31bbe8e304ec2a2c38fcc52a5716e
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2022-01-16 16:07:38 +0700

    Add server, healthcheck endpoint, and logger

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,27 @@
-console.log("Hello world");
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from 'cors';
+import config from 'config';
+import logger from './utils/logger';
+
+const port = config.get<number>("port");
+const host = config.get<number>("host");
+const corsOrigin = config.get<string>("corsOrigin");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: corsOrigin,
+    credentials: true
+  }
+});
+
+app.get("/", (_, response) => {
+  response.send("Server is up");
+});
+
+httpServer.listen(port, host, () => {
+  logger.info(`Server is listening at port ${port}`);
+});
