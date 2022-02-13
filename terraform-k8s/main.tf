6c536837d4d22d065570aea604018e775cce6892
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2022-02-14 00:13:13 +0700

    Change kube-config generation process

--- terraform-k8s/main.tf
+++ terraform-k8s/main.tf
@@ -4,6 +4,12 @@ terraform {
     lock = true
     prefix = "/socketio-chat-app"
   }
+
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+  }
 }
 
 provider "google" {
@@ -55,17 +61,4 @@ module "gke" {
       disk_size_gb = 10
     }
   ]
-}
-
-module "gke_auth" {
-  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  depends_on = [module.gke]
-  project_id = var.project_id
-  location = module.gke.location
-  cluster_name = module.gke.name
-}
-
-resource "local_file" "kubeconfig" {
-  content = module.gke_auth.kubeconfig_raw
-  filename = "kube-config"
 }
